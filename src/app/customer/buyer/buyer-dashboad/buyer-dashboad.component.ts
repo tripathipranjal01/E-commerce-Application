112b2cd0cff99a34cad3069a540fa45d28833b04
Pranjal Tripathi <tripathipranjal01@gmail.com>
2024-06-23 00:44:51 +0530

    feat: Add initial files and components for product, contact-us, user-crud, user-profile, checkout, admin-dashboard, buyer-dashboard, page-not-found, seller-dashboard, user.service, product.service, customer.service, core.module, http.interceptor, main.ts, admin.module, shared.module, customer.module, tsconfig.app.json, and .editorconfig

---
+++ src/app/customer/buyer/buyer-dashboad/buyer-dashboad.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buyer-dashboad',
+  standalone: true,
+  imports: [],
+  templateUrl: './buyer-dashboad.component.html',
+  styleUrl: './buyer-dashboad.component.css'
+})
+export class BuyerDashboadComponent {
+
+}
